bdec89a267295714b3983cd90f7c8359a41d6e70
Ben Rooke <ben_rooke@icloud.com>
2018-03-17 15:57:31 -0700

    Using the GMT timezone to match what Perfectmind expects, so newer records do not get inserted over and over again.

--- Studio Kicks Leaderboard/Studio Kicks Leaderboard/Util.swift
+++ Studio Kicks Leaderboard/Studio Kicks Leaderboard/Util.swift
@@ -23,16 +23,26 @@ class Util {
   }
 
   static func databaseEpoch(from string: String?) -> Int64? {
-    guard let string = string?.components(separatedBy: ".")[0] else {
+    guard let dateString = string?.components(separatedBy: ".")[0] else {
       return nil
     }
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-    let date = formatter.date(from: string)
+    let date = formatter.date(from: dateString)
     guard let timeInterval = date?.timeIntervalSince1970 else {
       return nil
     }
     return Int64(timeInterval)
+  }
+
+  static func timestamp(from date: Date?) -> String? {
+    guard let date = date else {
+      return nil
+    }
+    let formatter = DateFormatter()
+    formatter.timeZone = TimeZone(abbreviation: "GMT")
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    return formatter.string(from: date)
   }
 
 }
